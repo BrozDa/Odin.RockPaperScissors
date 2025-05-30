434ce74f7f58bf75a5efeb1674ac1c2f516617ff
Brozda <daniel.brozik@gmail.com>
2025-05-30 22:06:27 +0200

    feat: Implement Rock, Paper, Scissors Game
    Implemented simple alg which continues until either computer or player
    won 5 games.
    Computer choice is randomly generated whereas player choice is taken
    from user input. User input includes simple validation.

--- javascript.js
+++ javascript.js
@@ -1,16 +1,64 @@
+let computerScore = 0;
+let playerScore = 0;
+
 function getComputerChoice(){
-  let number =  Math.floor(Math.random()*3);
+  let choiceNumber =  Math.floor(Math.random()*3);
 
-  if(number === 0){
-    return "Rock";
+  if(choiceNumber === 0){
+    return "rock";
   }
-  if(number === 1){
-    return "Paper"
+
+  if(choiceNumber === 1){
+    return "paper"
   }
-  return "Scissors";
+  return "scissors";
 }
+function getHumanChoice(){
+  let choice = prompt("Enter your choice: (Rock, Paper, Scissors)").toLowerCase();
 
-for(let i =0; i<10;i++){
+  while(choice != "rock" &&  choice != "paper" && choice != "scissors" ){
+
+    choice = prompt("Enter VALID choice: (Rock, Paper, Scissors)").toLowerCase()
+  }
+  return choice;
+}
+function playRound(computerChoice, playerChoice){
+  console.log(computerChoice);
+  console.log(playerChoice);
+
+  if(computerChoice === playerChoice){
+    console.log("It's a tie!");
+  }
+  else if((computerChoice === "paper" && playerChoice === "rock")
+    || (computerChoice === "scissors" && playerChoice === "paper")
+    || (computerChoice === "rock" && playerChoice === "scissors"))
+  {
+      console.log(`Computer won! ${computerChoice} beats ${playerChoice}`);
+      computerScore++;
+  }
+  else{
+    console.log(`Player won! ${playerChoice} beats ${computerChoice}`);
+    playerScore++;
+  }
+
+}
+function evaluateGame()
+{
+  if(computerScore > playerScore){
+    console.log(`Computer won with score ${computerScore}, player got only ${playerScore} points`)
+  }
+  else{
+    console.log(`Player won with score ${playerScore}, computer got only ${computerScore} points`)
+  }
+}
+function playGame(){
+  while(computerScore <5 && playerScore < 5){
+    playRound(getComputerChoice(), getHumanChoice());
+  }
   
-  console.log(getComputerChoice());
+  evaluateGame();
+
 }
+
+playGame();
+
